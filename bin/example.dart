bed6cb6bdf2daa806517879df459bba4d29ac816
swathi Krishna TS <swathikrishna2592@gmail.com>
2024-02-13 10:47:00 +0530

    first commit

---
+++ bin/example.dart
@@ -0,0 +1,13 @@
+class Adress{
+  String? name;
+  String? place;
+  int? age;
+  int? pin;
+  Adress({this.name,this.place,this.age,this.pin});
+
+  void display()
+  {
+    print("name=$name \n place=$place \n age=$age \n pin=$pin");
+
+  }
+}
---
+++ bin/sample1.dart
@@ -0,0 +1,10 @@
+import 'example.dart';
+
+void main()
+{
+  Adress adress=Adress(name: 'swathi',age: 23,place:'kongad',pin: 678631);
+  adress.name;
+  adress.place;
+  adress.pin;
+  adress.display();
+}
